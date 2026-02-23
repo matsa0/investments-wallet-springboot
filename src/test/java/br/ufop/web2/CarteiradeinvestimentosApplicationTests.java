73dcf82b3cd5129cefb640f28453009785023808
Matheus Azevedo <az3vedomatheus@gmail.com>
2026-02-23 10:33:10 -0300

    feat(project): add fresh new project

---
+++ src/test/java/br/ufop/web2/CarteiradeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufop.web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiradeinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
